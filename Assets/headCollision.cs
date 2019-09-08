6170d0f9203e4603f2549ad113fd43a0e41fe2ed
Luca Klingenberg <klingeluca@gmail.com>
2019-09-08 11:20:12 +0200

    PEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEENIS

---
+++ Assets/headCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class headCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider c){
+      if(c.tag == "destroyable"){
+           Destroy(c.gameObject);
+      }
+    }
+}
